e6c2189a608df86159bc4e409ed5ca0428f13916
Prandown <140757974+Prandan127@users.noreply.github.com>
2025-09-13 14:34:09 +0300

    laba2task3 changed
    removed "th", "nd" and other shit from tests

--- laba2/task3.cs
+++ laba2/task3.cs
@@ -4,18 +4,6 @@ public class Kata
 {
     public static string WhatCentury(string year)
     {
-        int y = int.Parse(year);
-        int century = (y + 99) / 100;
-        
-        if (century % 100 >= 11 && century % 100 <= 13)
-            return century + "th";
-            
-        switch (century % 10)
-        {
-            case 1: return century + "st";
-            case 2: return century + "nd";
-            case 3: return century + "rd";
-            default: return century + "th";
-        }
+        return Convert.ToString((int.Parse(year) + 99) / 100);
     }
 }

6048393f0ca7e06f161a63afc7b39ca45e67d452
Prandown <140757974+Prandan127@users.noreply.github.com>
2025-09-13 14:49:59 +0300

    add laba2task14

---
+++ laba2/task14.js
@@ -0,0 +1,9 @@
+function isCircleSorted(arr){
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[(i + 1) % arr.length]) {
+      count++;
+    }
+  }
+  return count <= 1;
+}

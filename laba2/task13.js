e28ea831e9bf467e1fe77fa49d3c5e78c3034aff
Prandown <140757974+Prandan127@users.noreply.github.com>
2025-09-13 14:49:03 +0300

    add laba2task13

---
+++ laba2/task13.js
@@ -0,0 +1,8 @@
+function hexStringToRGB(hexString) {
+  const hex = hexString.replace('#', '');
+  return {
+    r: parseInt(hex.substring(0, 2), 16),
+    g: parseInt(hex.substring(2, 4), 16),
+    b: parseInt(hex.substring(4, 6), 16)
+  };
+}

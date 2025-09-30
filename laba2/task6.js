ee4ba3d9482deb425ee8c7cd98d3938cd50423d0
Prandown <140757974+Prandan127@users.noreply.github.com>
2025-09-30 14:30:20 +0300

    edit task6

--- laba2/task6.js
+++ laba2/task6.js
@@ -1,28 +1,11 @@
-using System;
-
-public class Kata
-{
-    public static string ToWeirdCase(string s)
-    {
-        char[] chars = s.ToCharArray();
-        int index = 0;
-        
-        for (int i = 0; i < chars.Length; i++)
-        {
-            if (chars[i] == ' ')
-            {
-                index = 0;
-                continue;
-            }
-            
-            if (index % 2 == 0)
-                chars[i] = char.ToUpper(chars[i]);
-            else
-                chars[i] = char.ToLower(chars[i]);
-                
-            index++;
-        }
-        
-        return new string(chars);
-    }
+function toWeirdCase(string) {
+    return string.split(' ')
+                 .map(word => 
+                     word.split('')
+                         .map((char, index) => 
+                             index % 2 === 0 ? char.toUpperCase() : char.toLowerCase()
+                         )
+                         .join('')
+                 )
+                 .join(' ');
 }
